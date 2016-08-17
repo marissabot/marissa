ba50327199222c61d23a27fcf0143a31aee4a6c7
Ed Lewis <edward.richard.lewis@gmail.com>
2016-08-17 21:37:11 +0100

    Use libmarissa instead of hardcoded core

--- src/main/java/org/marissa/lib/RoutingEventListener.java
+++
@@ -1,9 +0,0 @@
-package org.marissa.lib;
-
-import co.paralleluniverse.fibers.SuspendExecution;
-import co.paralleluniverse.fibers.Suspendable;
-
-public interface RoutingEventListener {
-    @Suspendable
-    void routingEvent(String trigger, Response response) throws InterruptedException, SuspendExecution;
-}
---
+++ src/main/java/org/marissabot/marissa/Main.java
@@ -0,0 +1,74 @@
+package org.marissabot.marissa;
+
+import co.paralleluniverse.fibers.SuspendExecution;
+
+import java.util.Arrays;
+import org.marissabot.libmarissa.*;
+import org.marissabot.marissa.lib.Persist;
+import org.marissabot.marissa.modules.Animate;
+import org.marissabot.marissa.modules.Search;
+import org.marissabot.marissa.modules.define.Define;
+import org.marissabot.marissa.modules.scripting.ScriptEngine;
+import org.marissabot.marissa.modules.MiscUtils;
+import org.marissabot.marissa.modules.Score;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rocks.xmpp.core.XmppException;
+
+public class Main {
+    
+    public static void main(String[] args) throws XmppException, SuspendExecution, InterruptedException {
+
+        String username = Persist.load("core", "userid");
+        String password = Persist.load("core", "password"); 
+        String nickname = Persist.load("core", "nickname");
+        final String joinRoom = Persist.load("core", "joinroom");
+
+        Router router = new Router( "(?i)@?"+nickname );
+
+        router.on(".*time.*", MiscUtils::tellTheTime);
+        router.on("selfie", MiscUtils::selfie);
+        router.on("ping", MiscUtils::ping);
+        router.on("echo.*", MiscUtils::echo);
+
+        router.on(".*", ScriptEngine::dispatchToAll);
+
+        router.on("define\\s+.*", Define::defineWord);
+
+        router.on("(search|image)\\s+.*", Search::search);
+        router.on("animate\\s+.*", Animate::search);
+
+        router.on("[-+]\\d+", Score::scoreChange);
+        router.on("score", Score::scores);
+        router.whenContains("[-+]\\d+\\s+(?i)@?"+nickname,
+                            (trigger,response) -> {
+                                String noNick = trigger.replaceAll("(?i)@?"+nickname, "");
+                                Score.scoreChange(noNick, response);
+                            });
+
+        Marissa marissa = new Marissa(
+            username,
+            password,
+            nickname,
+            Arrays.asList(new String[]{joinRoom})
+        );
+
+        Runtime.getRuntime().addShutdownHook(
+            new Thread() {
+                 @Override
+                 public void run() {
+                     Logger l = LoggerFactory.getLogger(Main.class);
+                     l.debug("Shutdown hook triggered");
+                     marissa.disconnect();
+                     l.debug("Shutdown hook completed");
+                 }
+             }
+        );
+
+        LoggerFactory.getLogger(Main.class).info("Launching...");
+
+        marissa.activate(router);
+
+    }
+    
+}
